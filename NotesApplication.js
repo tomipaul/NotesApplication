1ae4a2a4674f7bac89bff81cc32b32d624ef65fc
tomipaul <tomipaul95@gmail.com>
2016-08-17 12:54:41 +0100

    Update NotesApplication.js
    **added** '_note' to delete, get and edit
    **added** semicolon at line 17
    **added** search method
    **added** semicolon at line 41

--- NotesApplication.js
+++ NotesApplication.js
@@ -14,18 +14,28 @@ class NotesApplication {
 	listNotes() {
 		for (var itemIndex in this.notes;) {
 			var item = this.notes[itemIndex];
-			console.log('\nNote ID:',itemIndex,'\n', item.toString(),'\n\nBy Author', this.author)
+			console.log('\nNote ID:',itemIndex,'\n', item.toString(),'\n\nBy Author', this.author);
 		}
 	}
 	
-	get(note_id) {
+	get_note(note_id) {
 		return this.notes[note_id].toString();
 	}
 	
-	delete(note_id) {
+	search(search_text) {
+		for (var itemIndex in this.notes) {
+			var item = this.notes[itemIndex];
+			if (item.toString().includes(search_text)) {
+				console.log('\nNote ID:',itemIndex,'\n', item.toString(),'\n\nBy Author', this.author);
+			}
+		}
+	}
+	
+	delete_note(note_id) {
 		this.notes.splice(note_id, 1);
 	}
 	
-	edit(note_id, new_content) {
+	edit_note(note_id, new_content) {
 		this.notes[note_id] = new_content;
 	}
+}
